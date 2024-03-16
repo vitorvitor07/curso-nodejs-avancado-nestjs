aad141f6475bc3a7b465346e5166c82974dd54c5
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-03-16 15:24:30 -0300

    UserEntity contructor unit tests

--- nestjs-clean-arch/src/users/domain/entities/user.entity.ts
+++ nestjs-clean-arch/src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createAt = this.props.createAt || new Date()
+  }
+}
