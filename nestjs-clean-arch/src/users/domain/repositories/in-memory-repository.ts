4d3decd9f82c6ecf06008651114565aeb7452351
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-06-25 15:21:35 -0300

    adding repository InMemoryRepository

---
+++ nestjs-clean-arch/src/users/domain/repositories/in-memory-repository.ts
@@ -0,0 +1,47 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { RespositoryInterface } from './repository-contracts'
+
+export abstract class InMomeoryRepository<E extends Entity>
+  implements RespositoryInterface<E>
+{
+  itens: E[] = []
+
+  async insert(entity: E): Promise<void> {
+    this.itens.push(entity)
+  }
+
+  async findById(id: string): Promise<E> {
+    return this._get(id)
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.itens
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id)
+
+    const index = this.itens.findIndex(item => item.id === entity.id)
+
+    this.itens[index] = { id: entity.id, ...entity }
+  }
+
+  async delete(entity: E): Promise<void> {
+    await this._get(entity.id)
+
+    const index = this.itens.findIndex(item => item.id === entity.id)
+
+    this.itens.slice(index, 1)
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const _id = `${id}`
+
+    const entity = this.itens.find(item => item.id === _id)
+
+    if (!entity) throw new NotFoundError('Entity not found')
+
+    return entity
+  }
+}
