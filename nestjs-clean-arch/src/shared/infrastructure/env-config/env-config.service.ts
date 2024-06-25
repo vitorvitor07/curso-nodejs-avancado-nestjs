1d3b35030114f8178e37ddad086259e28da0ab85
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-06-25 11:43:40 -0300

    adding validation class error

--- nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.service.ts
+++ nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
-import { EnvConfig } from './env-config.interface'
 import { ConfigService } from '@nestjs/config/dist'
+import { EnvConfig } from './env-config.interface'
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
