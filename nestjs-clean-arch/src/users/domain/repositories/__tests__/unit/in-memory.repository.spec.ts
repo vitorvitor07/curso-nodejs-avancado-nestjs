f9f8bdcbf5425a94278ea84df3fc95dda49c4dc4
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-06-25 16:03:52 -0300

    InMemoryRepository unit tests - findById Method

--- nestjs-clean-arch/src/users/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ nestjs-clean-arch/src/users/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Entity } from '@/shared/domain/entities/entity'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
 import { InMomeoryRepository } from '../../in-memory-repository'
 
 type StubEntityProps = {
@@ -23,5 +24,19 @@ describe('InMomeoryRepository unit tests', () => {
     await sut.insert(entity)
 
     expect(entity.toJSON()).toStrictEqual(sut.itens[0].toJSON())
+  })
+
+  it('Should throw error when entity not found', async () => {
+    await expect(sut.findById('fakeId')).rejects.toThrow(NotFoundError)
+  })
+
+  it('Should find a entity by id', async () => {
+    const entity = new StubEntity({ name: 'Test Name', price: 50 })
+
+    await sut.insert(entity)
+
+    const result = await sut.findById(entity.id)
+
+    expect(entity.toJSON()).toStrictEqual(result.toJSON())
   })
 })
