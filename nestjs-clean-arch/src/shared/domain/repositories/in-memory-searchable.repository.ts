b120173b8e81cc3849c068af13b8fc58dcb0e213
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-07-19 10:37:40 -0300

    Adding serach method inMemorySearchableRepositoryClass

--- nestjs-clean-arch/src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ nestjs-clean-arch/src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,31 @@
 import { Entity } from '@/shared/domain/entities/entity'
 import { InMemoryRepository } from './in-memory-repository'
-import { SearchableRespositoryInterface } from './searchable-repository-contracts'
+import {
+  SearchableRespositoryInterface,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contracts'
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRespositoryInterface<E, any, any>
 {
-  search(props: any): Promise<any> {
-    throw new Error('Method not implemented.')
-  }
+  search(props: SearchParams): Promise<SearchResult<E>> {}
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
