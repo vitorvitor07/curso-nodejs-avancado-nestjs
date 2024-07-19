e73a9f99c41f236765b5bf346a53a3c84a9ec5c3
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-07-19 12:02:08 -0300

    Adding unit tests InMemorySearchableRespository class

---
+++ nestjs-clean-arch/src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemorySearchableRepository } from '../../in-memory-searchable.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['name']
+
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) return items
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+}
+
+describe('InMemoryRepository unit tests', () => {
+  let sut: StubInMemorySearchableRepository
+
+  beforeEach(() => {
+    sut = new StubInMemorySearchableRepository()
+  })
+
+  describe('applyFilter method', () => {})
+
+  describe('applySort method', () => {})
+
+  describe('applyPaginate method', () => {})
+
+  describe('search method', () => {})
+
+  it('', async () => {
+    const entity = new StubEntity({ name: 'Test Name', price: 50 })
+
+    await sut.insert(entity)
+
+    expect(entity.toJSON()).toStrictEqual(sut.itens[0].toJSON())
+  })
+})
