39f0d04478a40774716203d60fa9192d096022d5
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-21 18:29:54 -0300

    Refactoring user repository - part 1

--- nestjs-clean-arch/src/users/domain/repositories/user.repository.ts
+++ nestjs-clean-arch/src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,25 @@
-import { SearchableRespositoryInterface } from '@/shared/domain/repositories/searchable-repository-contracts'
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRespositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contracts'
 import { UserEntity } from '../entities/user.entity'
 
-export interface UserRepository
-  extends SearchableRespositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity> // can throw conflict error
-  emailExists(email: string): Promise<void> // can throw conflict error
+export namespace UserRepository {
+  export type Filter = string
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> {}
+
+  export interface Repository
+    extends SearchableRespositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>
+    emailExists(email: string): Promise<void>
+  }
 }
