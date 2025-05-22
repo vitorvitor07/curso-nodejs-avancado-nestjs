d949c32e9c3cb69ad67185816bf3b0928eb9a0d8
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-22 15:08:38 -0300

    BcryptjsHashProvider unit tests

---
+++ nestjs-clean-arch/src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,29 @@
+import { BcrypthsHashProvider } from '../../bcryptjs-hash.provider'
+
+describe('UserInMomeoryRepository unit tests', () => {
+  let sut: BcrypthsHashProvider
+
+  beforeEach(() => {
+    sut = new BcrypthsHashProvider()
+  })
+
+  it('Should return encrypted password', async () => {
+    const password = 'testpass123'
+    const hash = await sut.generateHash(password)
+    expect(hash).toBeDefined()
+  })
+
+  it('Should return false on invalid password', async () => {
+    const password = 'testpass123'
+    const hash = await sut.generateHash(password)
+    const result = await sut.compareHash('fake', hash)
+    expect(result).toBeFalsy()
+  })
+
+  it('Should return true on valid password', async () => {
+    const password = 'testpass123'
+    const hash = await sut.generateHash(password)
+    const result = await sut.compareHash(password, hash)
+    expect(result).toBeTruthy()
+  })
+})
