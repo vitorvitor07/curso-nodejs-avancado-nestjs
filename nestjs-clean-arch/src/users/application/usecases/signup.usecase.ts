f869a2d3a13c5f1ae23c67dfa2114fd58c2cd81a
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-22 14:49:15 -0300

    Adding usecase SignupUseCase - part 2

--- nestjs-clean-arch/src/users/application/usecases/signup.usecase.ts
+++ nestjs-clean-arch/src/users/application/usecases/signup.usecase.ts
@@ -1,3 +1,7 @@
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { BadRequestError } from '../errors/bad-request-error'
+
 export namespace SingUpUseCase {
   export type Input = {
     name: string
@@ -14,6 +18,21 @@ export namespace SingUpUseCase {
   }
 
   export class UseCase {
-    async execute(input: Input): Promise<Output> {}
+    constructor(private readonly userRepository: UserRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { email, name, password } = input
+
+      if (!email || !name || !password)
+        throw new BadRequestError('Input data not provider')
+
+      await this.userRepository.emailExists(email)
+
+      const entity = new UserEntity(input)
+
+      await this.userRepository.insert(entity)
+
+      return entity.toJSON()
+    }
   }
 }
