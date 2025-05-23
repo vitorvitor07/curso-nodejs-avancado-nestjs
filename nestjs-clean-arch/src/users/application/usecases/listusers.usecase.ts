9118cf3df02810e7a437335477f532a2181243d8
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-23 11:32:34 -0300

    Adding ListUsersUseCase - part 2

--- nestjs-clean-arch/src/users/application/usecases/listusers.usecase.ts
+++ nestjs-clean-arch/src/users/application/usecases/listusers.usecase.ts
@@ -1,11 +1,16 @@
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dto/pagination-output'
 import { SearchInput } from '@/shared/application/dto/search-input'
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserOutput, UserOutputMapper } from '../dto/user-output.dto'
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput
 
-  export type Output = void
+  export type Output = PaginationOutput<UserOutput>
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private readonly userRepository: UserRepository.Repository) {}
@@ -13,7 +18,14 @@ export namespace ListUsersUseCase {
     async execute(input: Input): Promise<Output> {
       const param = new UserRepository.SearchParams(input)
       const searchResult = await this.userRepository.search(param)
-      return
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item =>
+        UserOutputMapper.toOutput(item),
+      )
+      return PaginationOutputMapper.toOutput(items, searchResult)
     }
   }
 }
