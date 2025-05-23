f9f1f7ccbc8d6e061ad0e5a466ef194dd67ad8ea
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-23 12:15:30 -0300

    ListUsersUseCase unit tests - part 1

---
+++ nestjs-clean-arch/src/users/application/usecases/__tests__/unit/listusers.usecases.spec.ts
@@ -0,0 +1,58 @@
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { ListUsersUseCase } from '../../listusers.usecase'
+
+describe('ListUsersUseCase unit tests', () => {
+  let sut: ListUsersUseCase.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new ListUsersUseCase.UseCase(repository)
+  })
+
+  it('To output method', async () => {
+    let result = new UserRepository.SearchResult({
+      items: [] as any,
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    })
+    let output = sut['toOutput'](result)
+
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    })
+
+    const entity = new UserEntity(UserDataBuilder({}))
+
+    result = new UserRepository.SearchResult({
+      items: [entity],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    })
+
+    output = sut['toOutput'](result)
+
+    expect(output).toStrictEqual({
+      items: [entity.toJSON()],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    })
+  })
+})
