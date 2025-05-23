7a1f0998140ee9d37d8eb387fcf29d94cf026c18
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-23 10:48:26 -0300

    GetUserUseCase unit tests

---
+++ nestjs-clean-arch/src/users/application/usecases/__tests__/unit/getuser.usecase.spec.ts
@@ -0,0 +1,38 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { GetUserUseCase } from '../../getuser.usecase'
+
+describe('GetUserUseCase unit tests', () => {
+  let sut: GetUserUseCase.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new GetUserUseCase.UseCase(repository)
+  })
+
+  it('Should throws error when entity not found', async () => {
+    expect(() => sut.execute({ id: 'fakeId' })).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should be able to get user profile', async () => {
+    const spyFindById = jest.spyOn(repository, 'findById')
+    const items = [new UserEntity(UserDataBuilder({}))]
+    repository.items = items
+
+    const result = await sut.execute({ id: items[0]._id })
+
+    expect(spyFindById).toHaveBeenCalled()
+    expect(result).toMatchObject({
+      id: items[0].id,
+      name: items[0].name,
+      email: items[0].email,
+      password: items[0].password,
+      createdAt: items[0].createdAt,
+    })
+  })
+})
