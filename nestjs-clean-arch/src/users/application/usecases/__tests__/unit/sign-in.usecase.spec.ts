3e6aa1b4f8eb952052d7e92ec65a5046f748c856
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-25 12:58:40 -0300

    Adding SigninUseCase unit tests

--- nestjs-clean-arch/src/users/application/usecases/__tests__/unit/sign-in.usecase.spec.ts
+++ nestjs-clean-arch/src/users/application/usecases/__tests__/unit/sign-in.usecase.spec.ts
@@ -1,42 +1,33 @@
 import { BadRequestError } from '@/shared/application/errors/bad-request-error'
+import { InvalidCredentialsError } from '@/shared/application/errors/invalid-credentials-error'
 import { HashProvider } from '@/shared/application/providers/hash-provider'
-import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
 import { BcrypthsHashProvider } from '@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
-import { SingUpUseCase } from '../../sign-up.usecase'
+import { SingInUseCase } from '../../sign-in.usecase'
 
-describe('SignupUseCase unit tests', () => {
-  let sut: SingUpUseCase.UseCase
+describe('SigninUseCase unit tests', () => {
+  let sut: SingInUseCase.UseCase
   let repository: UserInMemoryRepository
   let hashProvider: HashProvider
 
   beforeEach(() => {
     repository = new UserInMemoryRepository()
     hashProvider = new BcrypthsHashProvider()
-    sut = new SingUpUseCase.UseCase(repository, hashProvider)
+    sut = new SingInUseCase.UseCase(repository, hashProvider)
   })
 
   it('Should create a user', async () => {
-    const spyInsert = jest.spyOn(repository, 'insert')
-    const props = UserDataBuilder({})
-    const { email, password, name } = props
-    const result = await sut.execute({ email, password, name })
+    const spyInsert = jest.spyOn(repository, 'findByEmail')
+    const hashPassword = await hashProvider.generateHash('1234')
+    const entity = new UserEntity(
+      UserDataBuilder({ password: hashPassword, email: 'a@a.com' }),
+    )
+    repository.items = [entity]
+    const result = await sut.execute({ email: entity.email, password: '1234' })
     expect(spyInsert).toHaveBeenCalledTimes(1)
-    expect(result.id).toBeDefined()
-    expect(result.createdAt).toBeInstanceOf(Date)
-  })
-
-  it('Should not be able register whit same email twice', async () => {
-    const props = UserDataBuilder({ email: 'a@a.com' })
-    await sut.execute(props)
-    expect(() => sut.execute(props)).rejects.toBeInstanceOf(ConflictError)
-  })
-
-  it('Should throws error when name are not provided', async () => {
-    const props = Object.assign(UserDataBuilder({}), { name: null })
-
-    expect(() => sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+    expect(result).toStrictEqual(entity.toJSON())
   })
 
   it('Should throws error when password are not provided', async () => {
@@ -49,5 +40,17 @@ describe('SignupUseCase unit tests', () => {
     const props = Object.assign(UserDataBuilder({}), { email: null })
 
     expect(() => sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+
+  it('Should not be able authenticate with wrong password', async () => {
+    const hashPassword = await hashProvider.generateHash('1234')
+    const entity = new UserEntity(
+      UserDataBuilder({ email: 'a@a.com', password: hashPassword }),
+    )
+    repository.items = [entity]
+
+    await expect(() =>
+      sut.execute({ email: 'a@a.com', password: 'fake' }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
--- nestjs-clean-arch/src/users/infrastructure/users.module.ts
+++ nestjs-clean-arch/src/users/infrastructure/users.module.ts
@@ -1,9 +1,34 @@
+import { HashProvider } from '@/shared/application/providers/hash-provider'
 import { Module } from '@nestjs/common'
+import { SingUpUseCase } from '../application/usecases/sign-up.usecase'
+import { UserRepository } from '../domain/repositories/user.repository'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { BcrypthsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcrypthsHashProvider,
+    },
+    {
+      provide: SingUpUseCase.UseCase,
+      inject: ['UserRepository', 'HashProvider'],
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SingUpUseCase.UseCase(userRepository, hashProvider)
+      },
+    },
+  ],
 })
 export class UsersModule {}
