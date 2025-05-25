33339ce57dd8060f77f61d43d6ba17d84e9a72c3
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-25 12:17:05 -0300

    Adding SigninUseCase

---
+++ nestjs-clean-arch/src/users/application/usecases/sign-in.usecase.ts
@@ -0,0 +1,41 @@
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { BcrypthsHashProvider } from '@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
+import { BadRequestError } from '../../../shared/application/errors/bad-request-error'
+import { UserOutput, UserOutputMapper } from '../dto/user-output.dto'
+import { InvalidCredentialsError } from '@/shared/application/errors/invalid-credentials-error'
+
+export namespace SingInUseCase {
+  export type Input = {
+    email: string
+    password: string
+  }
+
+  export type Output = UserOutput
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private readonly userRepository: UserRepository.Repository,
+      private readonly hashProvider: BcrypthsHashProvider,
+    ) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { email, password } = input
+
+      if (!email || !password)
+        throw new BadRequestError('Input data not provider')
+
+      const entity = await this.userRepository.findByEmail(email)
+
+      const hashPassowrdMatches = await this.hashProvider.compareHash(
+        password,
+        entity.password,
+      )
+
+      if (!hashPassowrdMatches)
+        throw new InvalidCredentialsError('Invalid credentials')
+
+      return UserOutputMapper.toOutput(entity)
+    }
+  }
+}
