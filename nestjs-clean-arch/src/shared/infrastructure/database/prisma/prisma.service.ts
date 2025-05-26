4d575741a52c8e59f4dd060519b687903d3c6153
vitorvitor07 <vitorsilva_53@hotmail.com>
2025-05-26 11:18:31 -0300

    Refactoring prisma service

--- nestjs-clean-arch/src/shared/infrastructure/database/prisma/prisma.service.ts
+++ nestjs-clean-arch/src/shared/infrastructure/database/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from './generated/prisma'
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+}
