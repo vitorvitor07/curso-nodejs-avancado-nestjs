0772d6574cce9be278bb22a5d816d4f72b07c7b3
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-01-30 19:11:20 -0300

    feat(database): implement UserPrismaRepository and update users module

--- nestjs-clean-arch/src/users/infrastructure/users.module.ts
+++ nestjs-clean-arch/src/users/infrastructure/users.module.ts
@@ -1,4 +1,5 @@
 import { HashProvider } from '@/shared/application/providers/hash-provider'
+import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service'
 import { Module } from '@nestjs/common'
 import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase'
 import { GetUserUseCase } from '../application/usecases/get-user.usecase'
@@ -8,7 +9,7 @@ import { SignUpUseCase } from '../application/usecases/sign-up.usecase'
 import { UpdatePassordUseCase } from '../application/usecases/update-password.usecase'
 import { UpdateUserUseCase } from '../application/usecases/update-user.usecase'
 import { UserRepository } from '../domain/repositories/user.repository'
-import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { UserPrismaRepository } from './database/prisma/repositories/user-prisma.repository'
 import { BcrypthsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider'
 import { UsersController } from './users.controller'
 
@@ -16,8 +17,16 @@ import { UsersController } from './users.controller'
   controllers: [UsersController],
   providers: [
     {
+      provide: 'PrismaService',
+      useClass: PrismaService,
+    },
+    {
       provide: 'UserRepository',
-      useClass: UserInMemoryRepository,
+      // useClass: UserInMemoryRepository,
+      useFactory: (prismaService: PrismaService) => {
+        return new UserPrismaRepository(prismaService)
+      },
+      inject: ['PrismaService'],
     },
     {
       provide: 'HashProvider',
