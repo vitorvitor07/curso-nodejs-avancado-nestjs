f232abe288f54ee7231605e612ff2f597abfadfa
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-01-30 19:50:15 -0300

    feat(database): make DatabaseModule global and add forTest method for testing

--- nestjs-clean-arch/src/shared/infrastructure/database/database.module.ts
+++ nestjs-clean-arch/src/shared/infrastructure/database/database.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { PrismaClient } from '@prisma/client'
 import { EnvConfigModule } from '../env-config/env-config.module'
 import { PrismaService } from './prisma/prisma.service'
 
+@Global()
 @Module({
   imports: [EnvConfigModule.forRoot()],
   providers: [PrismaService, ConfigService],
   exports: [PrismaService],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forTest(prismaClient: PrismaClient) {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useFactory: () => prismaClient as PrismaService,
+        },
+      ],
+    }
+  }
+}
