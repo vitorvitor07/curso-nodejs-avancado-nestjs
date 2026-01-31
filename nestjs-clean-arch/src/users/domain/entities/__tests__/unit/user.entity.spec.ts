18d3f067f35e7b411309ff6a6d0dab1974289656
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-01-31 18:56:48 -0300

    Refactor UserEntity unit tests

--- nestjs-clean-arch/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ nestjs-clean-arch/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -19,9 +19,9 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined()
-    expect(sut.props.name).toEqual(props.name)
-    expect(typeof sut.props.name).toBe('string')
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
   })
 
   it('Setter of name field', () => {
@@ -31,14 +31,14 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined()
-    expect(sut.props.email).toEqual(props.email)
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
     expect(typeof sut.props.email).toBe('string')
   })
 
   it('Getter of password field', () => {
-    expect(sut.props.password).toBeDefined()
-    expect(sut.props.password).toEqual(props.password)
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
     expect(typeof sut.props.password).toBe('string')
   })
   it('Setter of password field', () => {
@@ -48,8 +48,8 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Getter of createdAt field', () => {
-    expect(sut.props.createdAt).toBeDefined()
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
   it('Should update a user', () => {
