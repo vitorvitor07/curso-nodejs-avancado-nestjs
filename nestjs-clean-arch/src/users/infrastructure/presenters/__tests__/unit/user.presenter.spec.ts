ac07fb53778c92b6e4af263ee177386e8d806b93
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-01 03:13:33 -0300

    UserPresenter class unit tests - part 2

--- nestjs-clean-arch/src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ nestjs-clean-arch/src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,6 +1,7 @@
+import { instanceToPlain } from 'class-transformer'
 import { UserPresenter } from '../../user.presenter'
 
-describe('UsersController', () => {
+describe('UserPresenter unit tests', () => {
   const createdAt = new Date()
   const props = {
     id: '310711f1-073b-461a-aa33-7ed0b0bac525',
@@ -9,14 +10,28 @@ describe('UsersController', () => {
     email: 'a@a.com',
     createdAt,
   }
+  let sut: UserPresenter
+
+  beforeEach(() => {
+    sut = new UserPresenter(props)
+  })
 
   describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props)
       expect(sut.id).toEqual(props.id)
       expect(sut.name).toEqual(props.name)
       expect(sut.email).toEqual(props.email)
       expect(sut.createdAt).toEqual(props.createdAt)
+    })
+  })
+
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut)
+    expect(output).toStrictEqual({
+      id: '310711f1-073b-461a-aa33-7ed0b0bac525',
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
     })
   })
 })
