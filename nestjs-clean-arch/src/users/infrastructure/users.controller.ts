43c2c7bc9974a492a3abec8ab09f955b94d50a0e
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-01 02:53:11 -0300

    Adding UserPresenter class

--- nestjs-clean-arch/src/users/infrastructure/users.controller.ts
+++ nestjs-clean-arch/src/users/infrastructure/users.controller.ts
@@ -11,6 +11,7 @@ import {
   Put,
   Query,
 } from '@nestjs/common'
+import { UserOutput } from '../application/dto/user-output.dto'
 import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase'
 import { GetUserUseCase } from '../application/usecases/get-user.usecase'
 import { ListUsersUseCase } from '../application/usecases/list-users.usecase'
@@ -23,6 +24,7 @@ import { SignInDto } from './dto/sign-in-user.dto'
 import { SignUpDto } from './dto/sign-up-user.dto'
 import { UpdatePasswordDto } from './dto/update-password-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { UserPresenter } from './presenters/user.presenter'
 
 @Controller('users')
 export class UsersController {
@@ -47,15 +49,21 @@ export class UsersController {
   @Inject(DeleteUserUseCase.UseCase)
   private deleteUserUseCase: DeleteUserUseCase.UseCase
 
+  static userToResponse(output: UserOutput) {
+    return new UserPresenter(output)
+  }
+
   @Post()
   async create(@Body() signUpDto: SignUpDto) {
-    return this.signUpUseCase.execute(signUpDto)
+    const output = await this.signUpUseCase.execute(signUpDto)
+    return UsersController.userToResponse(output)
   }
 
   @HttpCode(200)
   @Post('login')
   async login(@Body() signInDto: SignInDto) {
-    return this.signInUseCase.execute(signInDto)
+    const output = await this.signInUseCase.execute(signInDto)
+    return UsersController.userToResponse(output)
   }
 
   @Get()
@@ -65,12 +73,17 @@ export class UsersController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.getUserUseCase.execute({ id })
+    const output = await this.getUserUseCase.execute({ id })
+    return UsersController.userToResponse(output)
   }
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.updateUserUseCase.execute({ id, ...updateUserDto })
+    const output = await this.updateUserUseCase.execute({
+      id,
+      ...updateUserDto,
+    })
+    return UsersController.userToResponse(output)
   }
 
   @Patch(':id')
@@ -78,7 +91,11 @@ export class UsersController {
     @Param('id') id: string,
     @Body() updatePasswordDto: UpdatePasswordDto,
   ) {
-    return this.updatePasswordUseCase.execute({ id, ...updatePasswordDto })
+    const output = await this.updatePasswordUseCase.execute({
+      id,
+      ...updatePasswordDto,
+    })
+    return UsersController.userToResponse(output)
   }
 
   @HttpCode(204)
