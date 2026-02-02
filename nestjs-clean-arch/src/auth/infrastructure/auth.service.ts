61d7ac3ae8e25e5ef2519292986d80b68cf90678
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-02 13:10:42 -0300

    Implementing AuthService

--- nestjs-clean-arch/src/auth/infrastructure/auth.service.ts
+++ nestjs-clean-arch/src/auth/infrastructure/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { EnvConfigService } from '@/shared/infrastructure/env-config/env-config.service'
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 
+type GeneratedJwtProps = {
+  acessToken: string
+}
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private configService: EnvConfigService,
+  ) {}
+
+  async generateJwt(userId: string): Promise<GeneratedJwtProps> {
+    const acessToken = await this.jwtService.signAsync(userId, {})
+    return { acessToken }
+  }
+
+  async verifyJwt(token: string) {
+    return this.jwtService.verifyAsync(token, {
+      secret: this.configService.getJwtSecret(),
+    })
+  }
+}
