0637e254c10943ee2596e401c2fadf5f3f131755
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-02 11:11:41 -0300

    UsersController e2e tests - search method - part 2

--- nestjs-clean-arch/src/users/infrastructure/__tests__/e2e/search-user.e2e-spec.ts
+++ nestjs-clean-arch/src/users/infrastructure/__tests__/e2e/search-user.e2e-spec.ts
@@ -79,6 +79,70 @@ describe('UsersController e2e tests', () => {
       })
     })
 
+    it('should return users ondered by createdAt', async () => {
+      const entities: UserEntity[] = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'TeSt']
+      arrange.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...UserDataBuilder({}),
+            name: element,
+            email: `a${index}@a.com`,
+          }),
+        )
+      })
+      await prismaService.user.createMany({
+        data: entities.map(item => item.toJSON()),
+      })
+      let searchParams = {
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      }
+      let queryParams = new URLSearchParams(searchParams as any).toString()
+      let res = await request(app.getHttpServer())
+        .get(`/users/?${queryParams}`)
+        .expect(200)
+      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta'])
+      expect(res.body).toStrictEqual({
+        data: [entities[0], entities[4]].map(item =>
+          instanceToPlain(UsersController.userToResponse(item)),
+        ),
+        meta: {
+          total: 3,
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 2,
+        },
+      })
+
+      searchParams = {
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      }
+      queryParams = new URLSearchParams(searchParams as any).toString()
+      res = await request(app.getHttpServer())
+        .get(`/users/?${queryParams}`)
+        .expect(200)
+      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta'])
+      expect(res.body).toStrictEqual({
+        data: [entities[2]].map(item =>
+          instanceToPlain(UsersController.userToResponse(item)),
+        ),
+        meta: {
+          total: 3,
+          currentPage: 2,
+          perPage: 2,
+          lastPage: 2,
+        },
+      })
+    })
+
     it('should return a error with 422 code when the query params is invalid', async () => {
       const res = await request(app.getHttpServer())
         .get(`/users/?fakeId=10`)
