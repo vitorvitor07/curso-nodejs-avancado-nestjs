67732dadace686a54dd8994e3e573dab87cc3ecd
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-01 23:40:29 -0300

    Refactor UsersController e2e tests

--- nestjs-clean-arch/src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ nestjs-clean-arch/src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -55,16 +55,11 @@ describe('POST /users', () => {
       .send(signUpDto)
       .expect(201)
 
-    expect(Object.keys(res.body)).toStrictEqual([
-      'id',
-      'name',
-      'email',
-      'createdAt',
-    ])
+    expect(Object.keys(res.body)).toStrictEqual(['data'])
 
-    const user = await repository.findById(res.body.id)
+    const user = await repository.findById(res.body.data.id)
     const presenter = UsersController.userToResponse(user.toJSON())
     const serialized = instanceToPlain(presenter)
-    expect(res.body).toStrictEqual(serialized)
+    expect(res.body.data).toStrictEqual(serialized)
   })
 })
