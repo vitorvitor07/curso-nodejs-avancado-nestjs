3d426d87798f353e00844e6bf58c9416d2bffdbb
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-02 10:36:13 -0300

    UsersController e2e tests - remove method

---
+++ nestjs-clean-arch/src/users/infrastructure/__tests__/e2e/remove-user.e2e-spec.ts
@@ -0,0 +1,65 @@
+import { applyGlobalConfig } from '@/global-config'
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/__testing__/setup-prisma.tests'
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaClient } from '@prisma/client'
+import request from 'supertest'
+import { UsersModule } from '../../users.module'
+
+describe('UsersController e2e tests', () => {
+  let app: INestApplication
+  let module: TestingModule
+  let repository: UserRepository.Repository
+  const prismaService = new PrismaClient()
+  let entity: UserEntity
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile()
+    app = module.createNestApplication()
+    applyGlobalConfig(app)
+    app.init()
+    repository = module.get<UserRepository.Repository>('UserRepository')
+  })
+
+  beforeEach(async () => {
+    await prismaService.user.deleteMany()
+    entity = new UserEntity(UserDataBuilder({}))
+    await repository.insert(entity)
+  })
+
+  afterAll(() => {
+    module.close()
+  })
+
+  describe('DELETE /users/:id', () => {
+    it('should remove a user', async () => {
+      await request(app.getHttpServer())
+        .delete(`/users/${entity._id}`)
+        .expect(204)
+        .expect({})
+    })
+
+    it('should return a error with 404 when throw NoutFoundError with invalid Id', async () => {
+      await request(app.getHttpServer())
+        .get(`/users/fake-id`)
+        .expect(404)
+        .expect({
+          statusCode: 404,
+          error: 'Not Found',
+          message: 'UserModel not found using ID fake-id',
+        })
+    })
+  })
+})
