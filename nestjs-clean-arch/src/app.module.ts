4991c1d0622e7edbf4196c56c83cd05da6031b84
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-02-01 22:29:25 -0300

    Config e2e tests

--- nestjs-clean-arch/src/app.module.ts
+++ nestjs-clean-arch/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
-import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [EnvConfigModule, AppService],
+  imports: [UsersModule, DatabaseModule, EnvConfigModule],
 })
 export class AppModule {}
